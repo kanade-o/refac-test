196ecfd09f19c358ded19933b3fbdbd8849edce1
kanade-o <sotaohnuma@gmail.com>
2025-08-05 15:07:27 +0900

    fix greet.java

--- greet.java
+++ greet.java
@@ -1,5 +1,13 @@
-// Before
+// After
 public void printGreeting() {
+    sayHello();
+    sayNiceToMeetYou();
+}
+
+private void sayHello() {
     System.out.println("Hello, my name is Sota.");
+}
+
+private void sayNiceToMeetYou() {
     System.out.println("Nice to meet you!");
 }
